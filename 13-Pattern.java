4bdb8c200dc60caed3c6ce42c0a8255d9e393c01
Kushal815 <kk8152929@gmail.com>
2024-10-11 19:28:25 +0530

    ain

---
+++ 13-Pattern.java
@@ -0,0 +1,65 @@
+import java.util.*;
+
+class pattern {
+    public static void main(String[] args) {
+
+
+
+    
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("x axis :");
+        // int x= sc.nextInt();
+
+        // System.out.print("y axis :");
+        // int y= sc.nextInt();
+
+
+
+        // //Basic rectangular pattern
+        // for(int i=0;i<x;i++){
+        //     for(int j=0;j<y;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println(" ");
+        // }
+
+
+        // //Halow rectangular pattern
+        // for(int i = 0;i<y;i++){
+        //     for(int j=0;j<x;j++){
+        //         if (i==0 || i == y-1 || j==0 || j ==x-1) {
+        //             System.out.print("*");
+        //         }else{
+        //             System.out.print(" ");
+        //         }
+                
+        //     }
+        //     System.out.print("\n");
+        // }
+        // System.out.print("\n");
+
+
+        //Increaasing pattern
+    //     int n = 5;
+    //     // int number = 1;
+
+
+    //    for(int i=1; i<=n; i++) {
+    //        for(int j=1; j<=i; j++) {
+    //            System.out.print("#");
+    //         //    number++;
+    //        }
+    //        System.out.println();
+    //    }
+
+
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=4;j++){
+                if(j<=4-i)
+                    System.out.print(" ");
+                else System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
